151da9c6c67932fdb176fcb057779fd264e53e0f
Simeon Lukas <rtdust@gmail.com>
2022-10-15 08:36:16 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
 use regex::Regex;
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Welche Bibel soll es sein?");
+    println!(
+        "{0: <30} | {1: <30} | {2: <30} | {3: <30}",
+        "1. Einheitsübersetzung (EU)", "2. Lutherbibel (LUT)", "DBU", "ELB"
+    );
+    let mut version = String::new();
+    let mut buch = String::new();
+
+    io::stdin()
+        .read_line(&mut version)
+        .expect("Diese Version kenne ich nicht!");
+
+    version = version.trim().to_string();
+
+    if version == "1" || version == "EU" {
+        println!("Die Einheitsüberstzung wurde gewählt!");
+        version = "EU".to_string();
+        io::stdin()
+            .read_line(&mut version)
+            .expect("Welches Buch möchtest du lesen?");
+    }
+
+    if version == "2" || version == "LUT" {
+        println!("Die Lutherbibel wurde gewählt!");
+        version = "LUT".to_string();
+    }
+
     println!("{}", "Dokument wird erstellt!");
-    let mut file = File::create("Bibel.md").expect("Datei konnte leider nicht erstellt werden.");
+    let dateiname = format!("{version}-Bibel.md");
+    let mut file = File::create(&dateiname).expect("Datei konnte leider nicht erstellt werden.");
     let mut ausgabe = " ".to_string();
-    for n in 1..3 {
-        let url = "https://www.bibleserver.com/EU/Psalmen";
-        let urlfin = format!("{url}{n}");
+    for n in 1..2 {
+        let url = "https://www.bibleserver.com/";
+        let urlfin = format!("{url}{version}/{n}");
         let ergebnis: String = reqwest::get(urlfin).await?.text().await?.to_string();
         let split: Vec<&str> = ergebnis
             .split("<header style=\"grid-row-start: 1;grid-row-end: 2\">")
